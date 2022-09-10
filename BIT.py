45b98b6a28cc41e78c90bdcefc23f4f862d939fa
kakukakujirori <kakuda40611@gmail.com>
2022-09-10 16:23:45 +0900

    Update LCA so that it can find a diameter easily.

--- BIT.py
+++ BIT.py
@@ -2,6 +2,96 @@ import numpy as np
 from numba import jitclass, i8
 
 
+class BIT:
+    """
+    https://tjkendev.github.io/procon-library/python/range_query/bit.html
+    Binary index treeの実装
+    1-indexedの配列[a1, a2,...,an]に対して以下のクエリをO(logn)で行う:
+        1. aiにxを加える
+        2. 区間和 ai + a(i+1) + ... + aj の和を求める
+    isom法を使えば、
+        1. aiの値を取得する
+        2. 区間[i, j]の全ての数にxを加算する
+    もO(logN)で行うことができる
+    """
+
+    def __init__(self, n: int, mod: int = -1):
+        """
+        添字は1スタート、modは必要であれば設定しないと内部でオーバーフローする。
+        """
+        self.n = n
+        self.mod = mod
+        self.data = [0] * (n + 1)
+        self.el = [0] * (n + 1)
+
+    def add(self, i: int, x: int):
+        """
+        i>0に対してaiにxを加算(x < 0でもOK)
+        """
+        if i <= 0 or self.n < i:
+            raise ValueError("i should be within 1 to n")
+        else:
+            self.el[i] += x
+            if self.mod != -1: self.el[i] %= self.mod
+            while i <= self.n:
+                self.data[i] += x
+                if self.mod != -1:
+                    self.data[i] %= self.mod
+                i += i & -i
+
+    def sum(self, i: int = -1):
+        """
+        a1+a2+...+aiを求める。
+        i=Noneの場合はj=self.nとして計算される。
+        """
+        if i < 0: i = self.n
+        s = 0
+        while i > 0:
+            s += self.data[i]
+            if self.mod != -1: s %= self.mod
+            i -= i & -i # i&(-i)でiの最下位ビットのみ立った値を得る
+        return s
+
+    def get(self, i: int, j: int = -1):
+        """
+        ai+a(i+1)+...+ajを求める。
+        j=Noneの場合はj=self.nとして計算される。
+        """
+        if j < 0: j = self.n
+        assert i <= j
+        return self.sum(j) - self.sum(i - 1)
+
+    def binary_search(self, x: int):
+        """
+        a1+a2+...+ai <= x < a1+a2+...+ai+a(i+1)となるiを求める。
+        BITをmultisetの代替として使用する場合に使える。
+        数列は正の数からのみ成ることが前提。必要であれば下2行のチェックを省いて高速化すること。
+        """
+        for i in range(1, self.n):
+            assert self.get(i, i) >= 0
+
+        if self.get(1, self.n) <= x:
+            return self.n + 1
+        left = 1  # a1+...+a(left) <= x
+        right = self.n  # x < a1+...+a(right)
+        while right - left > 1:
+            mid = (left + right) // 2
+            if self.get(1, mid) <= x:
+                left = mid
+            else:
+                right = mid
+        return left
+
+    def debug(self):
+        """
+        BITが仮想的に見ている配列を返す
+        """
+        return [self.get(i) for i in range(1, self.n + 1)]
+
+
+################################################################
+
+
 spec = [
     ('n', i8),
     ('mod', i8),
@@ -10,7 +100,7 @@ spec = [
 ]
 
 @jitclass(spec)
-class BIT:
+class NumbaBIT:
     """
     https://tjkendev.github.io/procon-library/python/range_query/bit.html
     Binary index treeの実装
--- LeastCommonAncestor.py
+++ LeastCommonAncestor.py
@@ -3,24 +3,41 @@ class LCA():
     木の最小共通祖先(Least Common Ancestor)をダブリングで求める。
     頂点番号が0, 1, ..., n-1で与えられた木のrepnを入力としてインスタンス化、2頂点を入力してLCAを返す。
     """
-    def __init__(self, repn):
+    def __init__(self, repn, root=0):
         import sys
         sys.setrecursionlimit(10**8)
+        assert root >= 0
         self.n = len(repn)
         self.repn = repn
         self.log_depth = len(bin(self.n - 1)[2:])
-        self.parent = [[-1] * self.n for _ in range(self.log_depth)]  # parent[k][v] = (頂点vから2^k上の親)
-        self.depth = [0] + [-1] * (self.n - 1)  # 根（頂点0）からの距離
+        self.parent = None  # [[-1] * self.n for _ in range(self.log_depth)]  # parent[k][v] = (頂点vから2^k上の親)
+        self.depth = None  # [-1] * self.n
+        self.root = root
+        self.reroot(root)
         
+    def reroot(self, r=None):
+        """
+        Reroot the tree from r.
+        If r is None, r is set to be an endpoint of a diameter of the tree,
+        which can be useful when accounting for a diameter of the tree.
+        """
+        if r is None:
+            self.root = self.farthest_vertex()
+        
+        # initialize
+        self.depth = [-1] * self.n
+        self.parent = [[-1] * self.n for _ in range(self.log_depth)]
+        self.depth[self.root] = 0
+
         # dfs
         def dfs(v, p, d):
-            for nv in repn[v]:
+            for nv in self.repn[v]:
                 if nv == p: continue
                 self.parent[0][nv] = v
                 self.depth[nv] = d + 1
                 dfs(nv, v, d + 1)
         
-        dfs(0, -1, 0)
+        dfs(self.root, -1, 0)
 
         # doubling preprocess
         for k in range(self.log_depth - 1):
@@ -30,7 +47,25 @@ class LCA():
                 else:
                     self.parent[k + 1][v] = self.parent[k][self.parent[k][v]]
 
+    def farthest_vertex(self):
+        """
+        Return a farthest vertex from self.root
+        This can be useful when detecting a diameter (p, q) by calling
+        self.reroot(), and then (p, q) = (lca.root, lca.farthest_vertex())
+        """
+        v = 0
+        max_depth = -1
+        for i, x in enumerate(self.depth):
+            if x > max_depth:
+                v = i
+                max_depth = x
+        assert max_depth >= 0
+        return v
+
     def __call__(self, u, v):
+        """
+        Return LCA of u and v
+        """
         if self.depth[u] < self.depth[v]: u, v = v, u
         for k in range(self.log_depth):
             if ((self.depth[u] - self.depth[v]) >> k & 1):
